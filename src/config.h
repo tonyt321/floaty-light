aa412f6e2140703d604c8a16a6b79b10358e4be7
tonyt321 <gravesmarcello@gmail.com>
2023-07-06 20:37:02 +0000

    striped down can bus

--- src/config.h
+++ src/config.h
@@ -6,63 +6,8 @@
 // ###################################################################### //
 
 #define VESC_CAN_ID 25 // VESC-ID as configured in VESC as decimal
-// #define FAKE_VESC_ENABLED
 #define ERPM_IDLE 10
 
-#define LIGHT_NUMPIXELS 42 // the number of LEDs (first half front, second half back, needs to be even number)
-#define LIGHT_LED_TYPE NEO_GRBW + NEO_KHZ800 // Update to whatever LED types you have.
 
-// Boot light sequence
-#define LIGHT_BOOT_TIME_MS 1000 // Time to show boot light (0 to disable)
-#define LIGHT_BOOT_COLOR_RED 0
-#define LIGHT_BOOT_COLOR_GREEN 20
-#define LIGHT_BOOT_COLOR_BLUE 20
-#define LIGHT_BOOT_COLOR_WHITE 0
-
-// Stopped Color for foot pads
-#define LIGHT_STOP_NONE_COLOR_RED 5
-#define LIGHT_STOP_NONE_COLOR_GREEN 0
-#define LIGHT_STOP_NONE_COLOR_BLUE 0
-#define LIGHT_STOP_NONE_COLOR_WHITE 0
-#define LIGHT_STOP_ADC1_COLOR_RED 0
-#define LIGHT_STOP_ADC1_COLOR_GREEN 0
-#define LIGHT_STOP_ADC1_COLOR_BLUE 10
-#define LIGHT_STOP_ADC1_COLOR_WHITE 0
-#define LIGHT_STOP_ADC2_COLOR_RED 10
-#define LIGHT_STOP_ADC2_COLOR_GREEN 0
-#define LIGHT_STOP_ADC2_COLOR_BLUE 20
-#define LIGHT_STOP_ADC2_COLOR_WHITE 0
-
-// Running lights front and back
-#define LIGHT_FRONT_COLOR_RED 255
-#define LIGHT_FRONT_COLOR_GREEN 255
-#define LIGHT_FRONT_COLOR_BLUE 255
-#define LIGHT_FRONT_COLOR_WHITE 255
-#define LIGHT_REAR_COLOR_RED 255
-#define LIGHT_REAR_COLOR_GREEN 0
-#define LIGHT_REAR_COLOR_BLUE 0
-#define LIGHT_REAR_COLOR_WHITE 0
-#define LIGHT_IDLE_BRIGHTNESS 128
-
-#define LIGHT_BAR_ENABLED      // activates a light bar, comment if you don't have one
-#define LIGHT_BAR_NUMPIXELS 10 // the number of LEDS in the light bar
-// Which LEDs represent which values
-#define LIGHT_BAR_LED_INDEX_ADC1 {8,9}
-#define LIGHT_BAR_LED_INDEX_ADC2 {0,1}
-#define LIGHT_BAR_LED_INDEX_BATTERY_CHARGE {2,3,4,5,6,7}
-#define LIGHT_BAR_LED_INDEX_DUTY_CYCLE {}
-// Config options
-#define LIGHT_BAR_OFF_ERPM 10000
-// Color Options
-#define LIGHT_BAR_ADC_ON_COLOR_RED 0
-#define LIGHT_BAR_ADC_ON_COLOR_GREEN 0
-#define LIGHT_BAR_ADC_ON_COLOR_BLUE 100
-#define LIGHT_BAR_ADC_ON_COLOR_WHITE 0
-#define LIGHT_BAR_ADC_OFF_COLOR_RED 30
-#define LIGHT_BAR_ADC_OFF_COLOR_GREEN 0
-#define LIGHT_BAR_ADC_OFF_COLOR_BLUE 100
-#define LIGHT_BAR_ADC_OFF_COLOR_WHITE 0
-#define LIGHT_BAR_BATTERY_BRIGHTNESS 30
-#define LIGHT_BAR_DUTY_CYCLE_BRIGHTNESS 30
 
 #endif //__CONFIG_H__
--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,15 @@
 #include <Arduino.h>
 #include <Logger.h>
 #include "config.h"
-#include "LightController.h"
-#include "LightBarController.h"
-#include <Adafruit_NeoPixel.h>
 #include "CanBus.h"
 
-int lastFake = 4000;
-int lastFakeCount = 0;
+
 VescData vescData;
-LightController *lightController;
-LightBarController *lightBarController;
 CanBus * canbus = new CanBus(&vescData);
 
 // Declare the local logger function before it is called.
 void localLogger(Logger::Level level, const char* module, const char* message);
 
-void fakeCanbusValues() {
-    if(millis() - lastFake > 3000) {
-        vescData.dutyCycle = random(0, 100);
-        if(lastFakeCount > 10) {
-            vescData.erpm = random(-100, 200);
-        } else {
-            vescData.erpm = 0;//random(-100, 200);
-        }
-        vescData.adc1 = 0.5;
-        vescData.adc2 = 0.5;
-        vescData.switchState = 0;
-        lastFake = millis();
-        lastFakeCount++;
-    }
-}
 
 void setup() {
   Logger::setOutputFunction(localLogger);
@@ -39,16 +18,10 @@ void setup() {
   if(Logger::getLogLevel() != Logger::SILENT) {
       Serial.begin(115200);
   }
-  lightController = new LightController(LIGHT_NUMPIXELS, LIGHT_PIN, LIGHT_LED_TYPE, &vescData);
-  lightBarController = new LightBarController(LIGHT_BAR_NUMPIXELS, LIGHT_BAR_PIN, LIGHT_LED_TYPE, &vescData);
   
   delay(50);
 
   canbus->init();
-  lightController->init();
-  #ifdef LIGHT_BAR_ENABLED
-  lightBarController->init();
-  #endif
 
   Logger::notice("Floaty", "Startup...");
 }
@@ -56,11 +29,6 @@ void setup() {
 void loop() {
 
   canbus->loop();
-  // call the led loops
-  lightController->loop();
-  #ifdef LIGHT_BAR_ENABLED
-  lightBarController->loop();
-  #endif
 
 }
 
